c965e5862283c2bac850a257c4cb58914980fea7
zxy <xiao_y_zhou@163.com>
2025-11-03 15:14:21 +0800

    [phase1] 1.数据准备（yf/stooq/synthtic & uci）2..训练usr 接受度model（使用logistic回归的监督学习模型），保存pipeline

---
+++ offline/cql/gen_datasets.py
@@ -0,0 +1,127 @@
+# fetch data
+import io
+import os
+import time
+import numpy as np
+import pandas as pd
+import requests
+import yfinance as yf
+from ucimlrepo import fetch_ucirepo
+
+DATA_DIR = "./data"
+# SPY：标普 500 指数 ETF
+# AGG：美国综合债券 ETF
+# SHY：短期国债 ETF
+_STOOQ_MAP = {"SPY": "spy.us", "AGG": "agg.us", "SHY": "shy.us"}
+
+def _fetch_data_from_yf(ticker, maxretry, base_sleep_second):
+    for attempt in range(1, maxretry + 1):
+        try:
+            data = yf.download(
+                ticker,
+                period = "10y",
+                interval = "1d",
+                progress=True, # 显示下载进度条看看
+                auto_adjust=False, # 不调整
+                threads=True, # 启用多线程
+            )
+            if data.empty or "Adj Close" not in data.columns:
+                raise ValueError(f"{ticker} 的数据是空的 or 缺少 'Adj Close' 列。")
+            return data["Adj Close"].rename(ticker)
+        except Exception as exc:
+            if attempt == maxretry:
+                raise RuntimeError(f"获取{ticker}失败") from exc
+            sleep_sec = base_sleep_second*attempt
+            print(f"yf err: {exc}, 尝试{attempt}/{maxretry}, {sleep_sec}s后重试....")
+            time.sleep(sleep_sec)
+    raise RuntimeError(f"yf 尝试太多次{ticker}")
+
+# 如果yf get失败，从stooq get数据，再失败就创建合成数据
+def _fetch_data_from_stooq(ticker):
+    symbol = _STOOQ_MAP.get(ticker)
+    url = f"https://stooq.com/q/d/l/?s={symbol}&i=d"
+    resp = requests.get(url, timeout = 35, headers={"User-Agent": "Mozilla/5.0"})# 绕过反爬
+    resp.raise_for_status()
+    df = pd.read_csv(io.StringIO(resp.text)) #io.StringIO创建虚拟文本流，从虚拟文件读数据
+    if df.empty or "Close" not in df.columns:
+        raise RuntimeError(f"stooq返回{ticker}空")
+    # rename(ticker): 将这一列数据命名为对应的股票代码（如 "SPY"）
+    s = df.set_index("Date")["Close"].rename(ticker).sort_index()
+    s.index = pd.to_datetime(s.index)
+    return s
+
+# 模拟生成一支股票在指定工作日内的价格走势，基于正态分布的每日收益率和复利计算，最终返回一个带有日期索引的价格序列。
+def _gen_synthetic_series(ticker):
+    base_price = {"SPY": 100.0, "AGG": 105.0, "SHY": 99.0}.get(ticker, 100.0)
+    days = 2520 # 10年的工作日
+    date_idx = pd.date_range(end=pd.Timestamp.today(), periods=days, freq="B")
+    # X % N 的结果永远在 0 到 N-1 之间。
+    # % (2 ** 32) 能确保最终得到的数字一定在 0 到 2**32 - 1 这个区间内
+    rng = np.random.default_rng(seed=abs(hash(ticker)) % (2 ** 32))
+    # 模拟每日收益率
+    # loc=0.0002：均值为 0.02%，表示每日预期收益率
+    # scale=0.009：标准差为 0.9%，表示收益率的波动程度
+    daily_returns = rng.normal(loc=0.0002, scale=0.009, size=len(date_idx))
+    price_series = base_price*(1 + daily_returns).cumprod() # cumprod计算复利
+    return pd.Series(price_series, index=date_idx, name=ticker)
+
+# 多层 fallback
+def download_market_data(maxretry: int = 5, base_sleep_sec: int = 60):
+    """
+    下载 SPY、AGG 和 SHY 过去 10 年的每日价格数据，并将数据保存为 CSV 文件
+    首选yf, 速率限制就用stooq, 不行就用合成数据
+    """
+    print("-----正在下载市场数据-----")
+    series_list = []
+
+    for ticker in ["SPY", "AGG", "SHY"]:
+        print(f"正在下载{ticker}...")
+        try:
+            series = _fetch_data_from_yf(ticker, maxretry, base_sleep_sec)
+            source = "yf"
+        except RuntimeError as e:
+            print(f"yf不可下载{ticker}：{e},尝试stooq...")
+            try:
+                series = _fetch_data_from_stooq(ticker)
+                source = "stooq"
+            except Exception as s_e:
+                print(f"stooq不可下载{ticker}：{s_e},尝试合成数据...")
+                series = _gen_synthetic_series(ticker)
+                source = "synthetic"
+            print(f"    {ticker} 从 {source} 下载成功, {len(series)} 行.")
+            series_list.append(series)
+
+        market_df = pd.concat(series_list, axis=1).sort_index()
+        output_path = os.path.join(DATA_DIR, "market_data.csv")
+        market_df.to_csv(output_path)
+        print(f"市场数据保存在 {output_path}")
+        return market_df
+
+def download_user_data():
+    """
+    下载 银行营销数据集(id222)
+    """
+    print("-----正在下载用户数据-----")
+    bank_m = fetch_ucirepo(id=222)
+    # 提取数据集中的特征数据（输入变量）包含客户的各种属性（如年龄、职业、收入等）
+    X = bank_m.data.features
+    # 提取目标变量（输出变量）通常是 "是否订阅定期存款" 这样的标签
+    y = bank_m.data.targets
+    b_m_full_df = pd.concat([X, y], axis=1)
+    output_path = os.path.join(DATA_DIR,"bm_full.csv")
+    b_m_full_df.to_csv(output_path, index=False, sep=';')
+    print(f"用户数据保存在 {output_path}")
+    return b_m_full_df
+
+def main():
+    "构造数据集"
+    if not os.path.exists(DATA_DIR):
+        os.makedirs(DATA_DIR)
+        print(f"创建文件夹: {DATA_DIR}")
+    download_market_data()
+    print("+"*30)
+    download_user_data()
+    print("\n数据集构造完毕")
+
+if __name__ == "__main__":
+    main()
---
+++ offline/cql/train_usr_model.py
@@ -0,0 +1,96 @@
+# 训练一个机器学习模型（logistic回归），用来预测客户是否会接受银行的定期存款推销。
+import os
+import pandas as pd
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.metrics import accuracy_score, classification_report
+
+# 定义文件路径
+DATA_FILE = "./data/bm_full.csv"  # 输入数据文件
+MODEL_DIR = "./models"  # 模型保存目录
+MODEL_PATH = os.path.join(MODEL_DIR, "user_model.pkl")  # 最终模型文件路径
+
+def get_model_features():
+    """
+    定义并返回用户接受度模型所使用的数值feature和类别feature
+    """
+    num_feature = ['age', 'balance', 'duration', 'campaign', 'pdays', 'previous']
+    categori_feature = ['job', 'marital', 'education', 'default', 'housing', 'loan', 'contact', 'day_of_week', 'month', 'poutcome']
+    return num_feature, categori_feature # 返回一个tuple
+
+def tarin_usr_accetp_model():
+    """
+    训练用户接受度模型，使用logistic 回归
+    """
+    print("-----开始训练用户接受度模型-----")
+
+    # 1. --------加载数据
+    if not os.path.exists(DATA_FILE):
+        raise FileNotFoundError(f"数据文件未找到，请先generate数据")
+    df = pd.read_csv(DATA_FILE, sep=';')
+    print(f"已经加载{len(df)}行数据")
+
+    # 2. --------数据预处理
+    # 清理原始数据集中列名可能包含的引号或空格
+    df.columns = [col.strip().replace('"', '') for col in df.columns]
+
+    # 目标变量是 'y'，表示客户是否接受了定期存款，接受1，不接受0
+    X = df.drop('y', axis=1)
+    y = df['y'].apply(lambda x: 1 if x.strip().replace('"', '') == 'yes' else 0)
+
+    # 获取特征列表
+    num_feature, categori_feature = get_model_features()
+
+    # 划分训练集和测试集，stratify=y 实现分层抽样，确保拆分后训练集与测试集中目标变量 y 的类别比例与原始数据集保持一致。
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+    # 为数值和类别特征创建预处理管道
+    num_transformer = StandardScaler()  # 指定具体的处理工具，数值特征标准化
+    categori_transformer = OneHotEncoder(handle_unknown='ignore')  # 类别特征独热编码
+
+    # 使用 ColumnTransformer 创建一个统一的预处理器
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('num', num_transformer, num_feature),
+            ('cat', categori_transformer, categori_feature)
+        ],
+        remainder='passthrough'  # 保留未指定的列（如果有的话）
+    )
+
+    # 创建包含预处理器和分类器的完整 Pipeline
+    model_pipline = Pipeline(steps=[
+        ('preprocessor',preprocessor),
+        ('classifire',LogisticRegression(class_weight='balanced', max_iter=1000,random_state=42))
+    ])
+
+    # 3. ---------训练模型
+    # 完成后，model_pipeline 就包含了已经用训练数据校准好的预处理器和已经训练好的逻辑回归模型
+    print("正在训练模型 Pipeline...")
+    model_pipline.fit(X_train, y_train)
+
+    # 4. ---------评估模型
+    print("\n-----------在测试集上评估模型性能:----------")
+    y_pred = model_pipline.predict(X_test)
+    print(f"准确率: {accuracy_score(y_test, y_pred):.4f}") #(预测正确的数量) / (总预测数量)
+    print("\n分类报告:")
+    print(classification_report(y_test, y_pred, target_names=['未接受', '接受']))
+
+    # 保存训练好的模型 Pipeline
+    if not os.path.exists(MODEL_DIR):
+        os.makedirs(MODEL_DIR)
+        print(f"已创建目录: {MODEL_DIR}")
+        
+    joblib.dump(model_pipline,MODEL_PATH)
+    # 模型预测的准确性严重依赖于新数据的预处理方式必须和训练数据完全一致
+    print(f"\n模型 Pipeline 已保存至: {MODEL_PATH}")
+
+def main():
+    tarin_usr_accetp_model()
+    print("\n用户接受度模型训练过程完成。")
+
+if __name__ == "__main__":
+    main()
